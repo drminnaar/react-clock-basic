ee2a49f90f1416c38b95329f461b5c25910687d3
drminnaar <douglas.minnaar@gmail.com>
2017-11-30 14:03:06 +0200

    Add missing header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Header = (props) => (
-    <nav className=" header navbar navbar-dark bg-dark">
+    <nav className="header navbar navbar-dark bg-dark">
         <div className="container">
             <div className="row m-auto">
                 <i className="fa fa fa-clock-o fa-4x text-white"></i>
-                <div className="h1 ml-2 my-auto text-light" href="/">{props.title}</div>
+                <div className="h1 ml-3 my-auto text-light" href="/">{props.title}</div>
             </div>
         </div>
     </nav>
