298fdd38e22c98b310a9db17803124ce71994eae
Jay Gordon <jaydestro@gmail.com>
2019-07-29 15:03:46 -0400

    minor test changes

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,28 +1,12 @@
 var assert = require('assert'),
     http = require('http');
 
-var server = require('../index.js');
 
 describe('/', function () {
   it('should return 200', function (done) {
     http.get('http://localhost:8080', function (res) {
       assert.equal(200, res.statusCode);
       done();
-    });
-  });
-
-  it('should say "React Clock"', function (done) {
-    http.get('http://localhost:8080', function (res) {
-      var data = '';
-
-      res.on('data', function (chunk) {
-        data += chunk;
-      });
-
-      res.on('end', function () {
-        assert.equal('React Clock', data);
-        done();
-      });
     });
   });
 });
